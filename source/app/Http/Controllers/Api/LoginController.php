3df4f5177af65269b0cdd39a349513d305f42a3b
Ivan Churkin <bahes.in@gmail.com>
2020-01-25 17:49:54 +0300

    feat: add api login controller

---
+++ source/app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use GuzzleHttp\Client;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request $request
+     *
+     * @return Response
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password', 'secret');
+
+        $http = new Client();
+
+        $response = $http->post('http://webserver.test/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'password',
+                'client_id' => '3',
+                'client_secret' => $credentials['secret'],
+                'username' => $credentials['email'],
+                'password' => $credentials['password'],
+                'scope' => '',
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
+    }
+}
--- source/routes/api.php
+++ source/routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('/login', 'Api\LoginController@login');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
